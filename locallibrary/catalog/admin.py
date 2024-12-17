fa9058555b7ce9468a506db3b8d68709b14c6915
andire120 <164429441+andire120@users.noreply.github.com>
2024-12-17 20:14:53 +0900

    fgh

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from catalog.models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+#id = andire300@gmail.com
+#password = a
